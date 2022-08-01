e2bfff7b6b15b8778671c24d2f533ee09d3dde19
Jose Arrillaga <jarrilla.services@gmail.com>
2022-07-31 21:00:56 -0400

    recv 25 -> 30

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ const MIN_USD_TRADE = 11;
 const USD_TRADE_QTY = 25;
 
 // The valid receive window for the request by binance us servers
-const RECV_WINDOW_MS = 25;
+const RECV_WINDOW_MS = 30;
 
 // delay before retrying a sell attempt
 const RETRY_DELAY_MS = 200;
